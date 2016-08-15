03a9dbe72f4a319730960091462df1ed02dd4506
Benedicte Raae <raae@bgraphic.no>
2016-08-16 00:06:24 +0200

    Imports and saves insta photos

---
+++ data.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var firebase = require("firebase");
+firebase.initializeApp({
+    serviceAccount: "dev.config.json",
+    databaseURL: "https://little-lilly-test.firebaseio.com"
+});
+
+var db = firebase.database();
+
+function fetchTestUser() {
+    return db.ref("users").child('ayRCeV8tYQUtiqrP8Y7SyI7QHa82').once("value");
+}
+
+function fetchUsers() {
+    return db.ref("users").once("value");
+}
+
+function savePhotos(uid, photos) {
+    var ref = db.ref("users/" + uid + "/letter/photos");
+    return ref.set(photos);
+}
+
+module.exports = {
+    fetchUsers: fetchUsers,
+    fetchTestUser: fetchTestUser,
+    savePhotos: savePhotos
+};
