fd517d301248f5d145dded81dea7da0289213d32
Benedicte Raae <raae@bgraphic.no>
2016-09-07 09:36:30 +0200

    Changed name of imported module

--- jobs/new-month.js
+++ jobs/new-month.js
@@ -1,4 +1,4 @@
 
-var photos = require('../app/letters.js');
+var letters = require('../app/letters.js');
 
-photos.moveLetterToInProgressForAllUsers();
+letters.moveLetterToInProgressForAllUsers();
